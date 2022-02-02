fc2efac5383046c86ac1e8bbbcefadd5e85bc62f
Vladislav <vladteen1@yandex.ru>
2022-02-02 22:41:31 +0300

    Отправка ответа на сообщение в тг

--- src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
+++ src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
@@ -1,15 +1,23 @@
 package com.example.vladteenbot.Controllers;
 
+import kong.unirest.Unirest;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TelegramApi {
-
+    final String telegramUrl = "https://api.telegram.org/bot";
     @RequestMapping("/telegramApi")
     public void telegram(@RequestBody String request)
     {
-        System.out.println(request);
+        System.out.print(request);
+        var response = Unirest.post(telegramUrl + System.getenv("BOT_TOKEN") + "/sendMessage")
+                .header("accept", "application/json")
+                .field("chat_id", "202867842")
+                .field("text", "Плазма форевер")
+                .asJson();
+        System.out.print(response.getBody().toString());
+
     }
 }
