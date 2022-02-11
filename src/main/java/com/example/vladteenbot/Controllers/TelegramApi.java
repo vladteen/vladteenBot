6a08276d1c10e061fcb65251a55bb4af74847e48
Vladislav <vladteen1@yandex.ru>
2022-02-11 16:53:17 +0300

    Добавили условия к вводу сообщений от пользователя, добавили клавиатуру

--- src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
+++ src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
@@ -2,6 +2,8 @@ package com.example.vladteenbot.Controllers;
 
 import com.pengrad.telegrambot.BotUtils;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.request.Keyboard;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +21,55 @@ public class TelegramApi {
         System.out.println("Входящий запрос:");
         System.out.println(request);
         var update = BotUtils.parseUpdate(request);
-        var sendMessageRequest = new SendMessage(update.message().chat().id(), "привет, " + update.message().from().firstName())
-        .replyToMessageId(update.message().messageId());
-        var sendMessageResponse = bot.execute(sendMessageRequest);
-        var response = BotUtils.toJson(sendMessageResponse);
-        System.out.println("Ответ на метод sendMessage:");
-        System.out.println(response);
 
+        // Достаём нужные поля
+        var chatId = update.message().from().id();
+        var userName = update.message().from().username();
+        var textFromUser = update.message().text();
+
+        var textFromBot = "";
+
+        if (textFromUser.equals("Привет")){
+            textFromBot = "Добро пожаловать, " + userName;
+        }else if(textFromUser.equals("/start")){
+            textFromBot = "Новая игра началась, выберите дальнейшее действие!";
+        }else{
+            textFromBot = "Не понятно, что вы от меня хотите! Напишите /start для начала новой игры";
+        }
+
+        // Создаём объект с типом сообщение от бота
+        var requestSendMessage = new SendMessage(chatId,textFromBot);
+
+        if (textFromUser.equals("/start")){
+            var keyboard = GetKeyboard();
+            requestSendMessage.replyMarkup(keyboard);
+        }
+
+        bot.execute(requestSendMessage);
+
+//        var sendMessageRequest = new SendMessage(update.message().chat().id(), "привет, " + update.message().from().firstName())
+//        .replyToMessageId(update.message().messageId());
+//        var sendMessageResponse = bot.execute(sendMessageRequest);
+//        var response = BotUtils.toJson(sendMessageResponse);
+//        switch (update.message().messageId()){
+//            case "/start":
+//                var sendMessage = new SendMessage(update.message().chat().id(), "привет, " + update.message().from().firstName())
+//                        .replyToMessageId(update.message().messageId());
+//        }
+//        System.out.println("Ответ на метод sendMessage:");
+//        System.out.println(response);
+
+    }
+
+    private Keyboard GetKeyboard(){
+        // Создаём объект с типом клавиатура
+        var keyboard = new ReplyKeyboardMarkup(
+                new String[]{"1 строка 1 столбец", "1 строка 2 столбец"},
+                new String[]{"2 строка 1 столбец", "1 строка 2 столбец"}
+        );
+        keyboard.oneTimeKeyboard(true);
+        keyboard.resizeKeyboard(true);
+
+        return keyboard;
     }
 }
