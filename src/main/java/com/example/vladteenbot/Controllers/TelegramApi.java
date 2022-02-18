bf3b5ca68c6d2f41220c54e8148b3f55d9c538cf
Vladislav <vladteen1@yandex.ru>
2022-02-18 18:27:43 +0300

    тест реализации сохранения состояний и корректное отображение кнопок

--- src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
+++ src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TelegramApi {
     TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
-
+    String state;
 
     @RequestMapping("/telegramApi")
     public void telegram(@RequestBody String request)
@@ -28,29 +28,73 @@ public class TelegramApi {
 
         var textFromBot = "";
 
-        switch (textFromUser){
-            case "Привет":
-                textFromBot = "Добро пожаловать, " + userName;
+        switch (state){
+            case "Новая игра":
+                switch (textFromUser){
+                    case "/start":
+                    case "Начать новую игру":
+                        textFromBot = "Новая игра началась, выберите дальнейшее действие!";
+                        state = "Нейтральный режим";
+                        break;
+                    default:
+                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /start для начала новой игры";
+                }
                 break;
-            case "/start":
-            case "Начать новую игру":
-                textFromBot = "Новая игра началась, выберите дальнейшее действие!";
+            case "Нейтральный режим":
+                switch (textFromUser){
+                    case "/rest":
+                    case "Отдыхать":
+                        textFromBot = "Даже самым лучшим войнам нужен отдых, выберите дальнейшее действие!";
+                        state = "Режим отдыха";
+                        break;
+                    case "/battle":
+                    case "Вступить в бой!":
+                        textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! Выберите дальнейщее действие!";
+                        state = "Режим боя";
+                        break;
+                    case "/info":
+                    case "Персонаж":
+                        textFromBot = "Воин: " + userName;
+                        state = "Нейтральный режим";
+                        break;
+                    default:
+                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /rest для отдыха, /battle для вступления в бой," +
+                                      "/info для информации о персонаже";
+                }
+            case "Режим отдыха":
+                switch (textFromUser){
+                    case "/finish_rest":
+                    case "Закончить отдых":
+                        textFromBot = "Мы полностью восстановились и готовы, выберите дальнейшее действие!";
+                        state = "Нейтральный режим";
+                        break;
+                    case "/info":
+                    case "Персонаж":
+                        textFromBot = "Воин: " + userName;
+                        state = "Режим отдыха";
+                        break;
+                    default:
+                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /finish_rest для завершения отдыха, " +
+                                "/info для информации о персонаже";
+                }
+            case "Режим боя":
+                switch (textFromUser){
+                    case "/battle":
+                    case "Вступить в бой!":
+                        textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! " +
+                                "Выберите дальнейщее действие!";
+                        state = "Режим боя";
+                        break;
+                    case "/kick":
+                    case "Ударить":
+                        textFromBot = "Вот это ударище О_О";
+                        state = "Режим боя";
+                        break;
+                        // Враг жив или нет, в зависимости от этого поменяем состояние
+                }
                 break;
-            case "/neutral":
-            case "Продолжить приключение":
-                textFromBot = "Приключение началось, выберите дальнейшее действие!";
-                break;
-            case "/rest":
-            case "Отдыхать":
-                textFromBot = "Даже самым лучшим войнам нужен отдых, выберите дальнейшее действие!";
-                break;
-            case "/battle":
-            case "Вступить в бой!":
-                textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! Выберите дальнейщее действие!";
-                break;
-            default:
-                textFromBot = "Не понятно, что вы от меня хотите! Напишите /start для начала новой игры";
         }
+
 
 //        if (textFromUser.equals("Привет")){
 //            textFromBot = "Добро пожаловать, " + userName;
@@ -69,17 +113,16 @@ public class TelegramApi {
         // Создаём объект с типом сообщение от бота
         var requestSendMessage = new SendMessage(chatId,textFromBot);
 
-        if (textFromUser.equals("/start")){
+        if (textFromUser.equals("/start") || textFromUser.equals("Начать новую игру")){
             var keyboard = GetKeyboardForStart();
             requestSendMessage.replyMarkup(keyboard);
-            textFromUser.equals("/neutral");
-        }else if (textFromUser.equals("/neutral")){
+        }else if (textFromUser.equals("/neutral") || textFromUser.equals("Нейтральный режим")){
             var keyboard = GetKeyboardForNeutral();
             requestSendMessage.replyMarkup(keyboard);
-        }else if (textFromUser.equals("/rest")){
+        }else if (textFromUser.equals("/rest") || textFromUser.equals("Режим отдыха")){
             var keyboard = GetKeyboardForRest();
             requestSendMessage.replyMarkup(keyboard);
-        }else if (textFromUser.equals("/battle")){
+        }else if (textFromUser.equals("/battle") || textFromUser.equals("Режим боя")){
             var keyboard = GetKeyboardForBattle();
             requestSendMessage.replyMarkup(keyboard);
         }else{
@@ -104,7 +147,7 @@ public class TelegramApi {
 
     private Keyboard GetKeyboardForStart(){
         // Создаём объект с типом клавиатура
-        var keyboard = new ReplyKeyboardMarkup(new String[]{"Начать новую игру", "Продолжить приключение"});
+        var keyboard = new ReplyKeyboardMarkup(new String[]{"Начать новую игру"});
         keyboard.oneTimeKeyboard(true);
         keyboard.resizeKeyboard(true);
         return keyboard;
@@ -113,30 +156,24 @@ public class TelegramApi {
         // Создаём объект с типом клавиатура
         var keyboard = new ReplyKeyboardMarkup(
                 new String[]{"Вступить в бой!", "Отдыхать"},
-                new String[]{"Персонаж", "test"}
+                new String[]{"Персонаж"}
         );
         keyboard.oneTimeKeyboard(true);
         keyboard.resizeKeyboard(true);
-
         return keyboard;
     }
     private Keyboard GetKeyboardForRest(){
         // Создаём объект с типом клавиатура
-        var keyboard = new ReplyKeyboardMarkup(new String[]{"Персонаж", "Закончить отдых"});
+        var keyboard = new ReplyKeyboardMarkup(new String[]{"Закончить отдых", "Персонаж"});
         keyboard.oneTimeKeyboard(true);
         keyboard.resizeKeyboard(true);
-
         return keyboard;
     }
     private Keyboard GetKeyboardForBattle(){
         // Создаём объект с типом клавиатура
-        var keyboard = new ReplyKeyboardMarkup(
-                new String[]{"Ударить", "Сдаться"},
-                new String[]{"Персонаж", "test"}
-        );
+        var keyboard = new ReplyKeyboardMarkup(new String[]{"Вступить в бой!", "Ударить"});
         keyboard.oneTimeKeyboard(true);
         keyboard.resizeKeyboard(true);
-
         return keyboard;
     }
 }
