a93e04c0ef5de9495a2bdd6cd7e3f905e9f16ffd
Vladislav <vladteen1@yandex.ru>
2022-02-19 18:56:06 +0300

    test

--- src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
+++ src/main/java/com/example/vladteenbot/Controllers/TelegramApi.java
@@ -28,89 +28,89 @@ public class TelegramApi {
 
         var textFromBot = "";
 
-        switch (state){
-            case "Новая игра":
-                switch (textFromUser){
-                    case "/start":
-                    case "Начать новую игру":
-                        textFromBot = "Новая игра началась, выберите дальнейшее действие!";
-                        state = "Нейтральный режим";
-                        break;
-                    default:
-                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /start для начала новой игры";
-                }
-                break;
-            case "Нейтральный режим":
-                switch (textFromUser){
-                    case "/rest":
-                    case "Отдыхать":
-                        textFromBot = "Даже самым лучшим войнам нужен отдых, выберите дальнейшее действие!";
-                        state = "Режим отдыха";
-                        break;
-                    case "/battle":
-                    case "Вступить в бой!":
-                        textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! Выберите дальнейщее действие!";
-                        state = "Режим боя";
-                        break;
-                    case "/info":
-                    case "Персонаж":
-                        textFromBot = "Воин: " + userName;
-                        state = "Нейтральный режим";
-                        break;
-                    default:
-                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /rest для отдыха, /battle для вступления в бой," +
-                                      "/info для информации о персонаже";
-                }
-                break;
-            case "Режим отдыха":
-                switch (textFromUser){
-                    case "/finish_rest":
-                    case "Закончить отдых":
-                        textFromBot = "Мы полностью восстановились и готовы, выберите дальнейшее действие!";
-                        state = "Нейтральный режим";
-                        break;
-                    case "/info":
-                    case "Персонаж":
-                        textFromBot = "Воин: " + userName;
-                        state = "Режим отдыха";
-                        break;
-                    default:
-                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /finish_rest для завершения отдыха, " +
-                                "/info для информации о персонаже";
-                }
-                break;
-            case "Режим боя":
-                switch (textFromUser){
-                    case "/battle":
-                    case "Вступить в бой!":
-                        textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! " +
-                                "Выберите дальнейщее действие!";
-                        state = "Режим боя";
-                        break;
-                    case "/kick":
-                    case "Ударить":
-                        textFromBot = "Вот это ударище О_О";
-                        state = "Режим боя";
-                        break;
-                        // Враг жив или нет, в зависимости от этого поменяем состояние
-                }
-                break;
-        }
-
-
-//        if (textFromUser.equals("Привет")){
-//            textFromBot = "Добро пожаловать, " + userName;
-//        }else if(textFromUser.equals("/start") || textFromUser.equals("Начать новую игру")){
-//            textFromBot = "Новая игра началась, выберите дальнейшее действие!";
-//        }else if(textFromUser.equals("/neutral")){
-//            textFromBot = "Приключение началось, выберите дальнейшее действие!";
-//        }else if(textFromUser.equals("/rest")){
-//            textFromBot = "Даже самым лучшим войнам нужен отдых, выберите дальнейшее действие!";
-//        }else if(textFromUser.equals("/battle")){
-//            textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! Выберите дальнейщее действие!";
-//        }else{
-//            textFromBot = "Не понятно, что вы от меня хотите! Напишите /start для начала новой игры";
+//        switch (state){
+//            case "Новая игра":
+//                switch (textFromUser){
+//                    case "/start":
+//                    case "Начать новую игру":
+//                        textFromBot = "Новая игра началась, выберите дальнейшее действие!";
+//                        state = "Нейтральный режим";
+//                        break;
+//                    default:
+//                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /start для начала новой игры";
+//                }
+//                break;
+//            case "Нейтральный режим":
+//                switch (textFromUser){
+//                    case "/rest":
+//                    case "Отдыхать":
+//                        textFromBot = "Даже самым лучшим войнам нужен отдых, выберите дальнейшее действие!";
+//                        state = "Режим отдыха";
+//                        break;
+//                    case "/battle":
+//                    case "Вступить в бой!":
+//                        textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! Выберите дальнейщее действие!";
+//                        state = "Режим боя";
+//                        break;
+//                    case "/info":
+//                    case "Персонаж":
+//                        textFromBot = "Воин: " + userName;
+//                        state = "Нейтральный режим";
+//                        break;
+//                    default:
+//                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /rest для отдыха, /battle для вступления в бой," +
+//                                      "/info для информации о персонаже";
+//                }
+//                break;
+//            case "Режим отдыха":
+//                switch (textFromUser){
+//                    case "/finish_rest":
+//                    case "Закончить отдых":
+//                        textFromBot = "Мы полностью восстановились и готовы, выберите дальнейшее действие!";
+//                        state = "Нейтральный режим";
+//                        break;
+//                    case "/info":
+//                    case "Персонаж":
+//                        textFromBot = "Воин: " + userName;
+//                        state = "Режим отдыха";
+//                        break;
+//                    default:
+//                        textFromBot = "Не понятно, что вы от меня хотите! Напишите /finish_rest для завершения отдыха, " +
+//                                "/info для информации о персонаже";
+//                }
+//                break;
+//            case "Режим боя":
+//                switch (textFromUser){
+//                    case "/battle":
+//                    case "Вступить в бой!":
+//                        textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! " +
+//                                "Выберите дальнейщее действие!";
+//                        state = "Режим боя";
+//                        break;
+//                    case "/kick":
+//                    case "Ударить":
+//                        textFromBot = "Вот это ударище О_О";
+//                        state = "Режим боя";
+//                        break;
+//                        // Враг жив или нет, в зависимости от этого поменяем состояние
+//                }
+//                break;
 //        }
+
+
+        if (textFromUser.equals("Привет")){
+            textFromBot = "Добро пожаловать, " + userName;
+        }else if(textFromUser.equals("/start") || textFromUser.equals("Начать новую игру")){
+            textFromBot = "Новая игра началась, выберите дальнейшее действие!";
+        }else if(textFromUser.equals("/neutral")){
+            textFromBot = "Приключение началось, выберите дальнейшее действие!";
+        }else if(textFromUser.equals("/rest")){
+            textFromBot = "Даже самым лучшим войнам нужен отдых, выберите дальнейшее действие!";
+        }else if(textFromUser.equals("/battle")){
+            textFromBot = "Наконец-то достойный противник, эта схватка будет легендарной! Выберите дальнейщее действие!";
+        }else{
+            textFromBot = "Не понятно, что вы от меня хотите! Напишите /start для начала новой игры";
+        }
 
         // Создаём объект с типом сообщение от бота
         var requestSendMessage = new SendMessage(chatId,textFromBot);
